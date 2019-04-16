a88f6c8c2e50f3b39212a3ca67dccec57b746dda
Young Gyun <kgiun1@naver.com>
2019-04-16 15:41:12 +0900

    Add files via upload

---
+++ unit-test-1week/src/test/java/UpperCaseCounterTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UpperCaseCounterTest {
+    private UpperCaseCounter upperCaseCounter = new UpperCaseCounter();
+
+    @Test
+    public void 널체크(){
+        String str = null;
+        int result = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        assertTrue( result ==0);
+    }
+
+    @Test
+    public void 빈값체크(){
+        String str = "";
+        int result = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+
+        assert(result == 0);
+    }
+
+    @Test
+    public void 대문자_ABC() {
+        String str = "ABC";
+        int result = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        System.out.println("result ::" + result);
+        assertTrue(result==3);
+    }
+
+}
